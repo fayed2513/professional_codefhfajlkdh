6487803103df506cba8f8418425f8179cd78b5f7
fayed <fayedhasan1000@gmail.com>
2024-10-04 20:50:55 +0600

    Step 6 done

--- StudentList.java
+++ StudentList.java
@@ -36,10 +36,8 @@ public class StudentList {
                 BufferedWriter s = new BufferedWriter(
                                    new FileWriter("students.txt", true));
                 String t = args[0].substring(1);
-                Date d = new Date();
-                String df = "dd/mm/yyyy-hh:mm:ss a";
-                DateFormat dateFormat = new SimpleDateFormat(df);
-                String fd = dateFormat.format(d);
+                DateFormat dateFormat = new SimpleDateFormat(Constants.Date_Format);
+                String fd = dateFormat.format(Constants.Date_Format);
                 s.write(", " + t + "\nList last updated on " + fd);
                 s.close();
             } catch (Exception e) {
